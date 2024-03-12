6e90099cb429814b231165b61481f8843d96074a
bfbrandhorst <bfbrandhorst@gmail.com>
2024-03-12 16:10:06 -0600

    File structure setup, outline for HTML, form.css and form.js

---
+++ assets/js/form.js
@@ -0,0 +1,16 @@
+const usernameInput = document.getElementById('username');
+const titleInput = document.getElementById('title');
+const contentInput = document.getElementById('content');
+
+function createAndRenderBlog() {
+    const blog = {
+        username: usernameInput.value,
+        title: titleInput.value,
+        content: contentInput.value,
+    };
+
+    const blogs = JSON.parse(localStorage.getItem('blogs'));
+    blogs.push(blog);
+    localStorage.setItem('blogs', JSON.stringify(blogs));
+
+}
