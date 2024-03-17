3d46c062c8e0749640da8a313867d8a4b1d24b12
bfbrandhorst <bfbrandhorst@gmail.com>
2024-03-17 15:41:04 -0600

    changes to dark mode botton on blog.html and blog css

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,11 @@
+const darkModeToggle =
+    document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
+
 function goBack() {
     window.history.go(-1);
 }
